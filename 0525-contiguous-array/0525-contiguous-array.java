31996d8afb7cee607ec83989dfdc97eedcbd9909
Vipul Anand <106806942+VipulAnand13@users.noreply.github.com>
2023-08-18 10:34:30 +0530

    Time: 20 ms (92.06%), Space: 54.6 MB (54.03%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.java
+++ 0525-contiguous-array/0525-contiguous-array.java
@@ -1,22 +1,43 @@
 class Solution {
     public int findMaxLength(int[] nums) {
         int n= nums.length;
-        int[] prefixSum= new int[n+1];
-        prefixSum[0]=0;
+//         int[] prefixSum= new int[n+1];
+//         prefixSum[0]=0;
         
-        for(int i=1; i<=n; i++){
-            prefixSum[i]= nums[i-1]==0? prefixSum[i-1]-1:prefixSum[i-1]+1;
+//         for(int i=1; i<=n; i++){
+//             prefixSum[i]= nums[i-1]==0? prefixSum[i-1]-1:prefixSum[i-1]+1;
+//             }
+        
+//         Map<Integer,Integer> map= new HashMap<Integer,Integer>();
+//         int ans=0;
+        
+//         for(int i=0;i<=n;i++){
+//             if(map.containsKey(prefixSum[i]))
+//                 ans= Math.max(ans,i-map.get(prefixSum[i]));
+//             else
+//                 map.put(prefixSum[i],i);
+//         }
+//         return ans;
+        
+        // Both codes( above and below are working): In one ) is inserted and other is checking              for '0' sum condition as well 
+        int[] prefixSum= new int[n];
+        prefixSum[0]= nums[0]==0 ? -1 : 1; 
+        for(int i=1; i<n; i++){
+            prefixSum[i]= nums[i]==0? prefixSum[i-1]-1:prefixSum[i-1]+1;
             }
         
         Map<Integer,Integer> map= new HashMap<Integer,Integer>();
         int ans=0;
         
-        for(int i=0;i<=n;i++){
+        for(int i=0;i<n;i++){
             if(map.containsKey(prefixSum[i]))
                 ans= Math.max(ans,i-map.get(prefixSum[i]));
+            else if(prefixSum[i]==0)     // if map contains 0 then subarray from 0 index
+                ans=Math.max(ans,i-0+1);
             else
                 map.put(prefixSum[i],i);
         }
         return ans;
+        
     }
 }
