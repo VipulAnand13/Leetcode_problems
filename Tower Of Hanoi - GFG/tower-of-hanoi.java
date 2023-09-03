14c405aa812a02e8e1594462b749ceaf8e2a90ca
Vipul Anand <106806942+VipulAnand13@users.noreply.github.com>
2023-09-04 01:12:11 +0530

    Added solution - LeetHub

--- Tower Of Hanoi - GFG/tower-of-hanoi.java
+++ Tower Of Hanoi - GFG/tower-of-hanoi.java
@@ -39,12 +39,22 @@ class Hanoi {
 
 
     long count=0;
+    // public long toh(int N, int from, int to, int aux){
+    //     if(N==0) return 0;
+    //     count=toh( N-1, from, aux, to);
+    //     System.out.println("move disk "+N+" from rod "+from+ " to rod "+ to);
+    //     count++;
+    //     count=count+toh( N-1, aux, to, from);
+        
+    //     return count;
+    // }
+    
     public long toh(int N, int from, int to, int aux){
         if(N==0) return 0;
-        count=toh( N-1, from, aux, to);
-        System.out.println("move disk "+N+" from rod "+from+ " to rod "+ to);
         count++;
-        count=count+toh( N-1, aux, to, from);
+        toh( N-1, from, aux, to);
+        System.out.println("move disk "+N+" from rod "+from+ " to rod "+ to);
+        toh( N-1, aux, to, from);
         
         return count;
     }
