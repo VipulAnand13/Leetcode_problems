8eb4c77978e96abf7ddccbdee6d72be093782566
Vipul Anand <106806942+VipulAnand13@users.noreply.github.com>
2023-09-04 01:52:03 +0530

    Added solution - LeetHub

---
+++ Search an Element in an array - GFG/search-an-element-in-an-array.java
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Main
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        
+        while(t-- > 0)
+        {
+            int sizeOfArray = Integer.parseInt(read.readLine());
+            int arr[] = new int[sizeOfArray];
+            
+            String st[] = read.readLine().trim().split("\\s+");
+            for(int i = 0; i < sizeOfArray; i++)
+              arr[i] = Integer.parseInt(st[i]);
+              
+            int x = Integer.parseInt(read.readLine());
+            
+            Solution obj = new Solution();
+            
+            System.out.println(obj.search(arr, sizeOfArray, x));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution{
+        
+    static int search(int arr[], int N, int X)
+    {
+        
+        // Your code here
+        int i=0;
+        int ans=-1;
+        for(int e: arr){
+            
+            if(e==X) {
+                ans=i;
+                break;
+                }
+            i++;    
+        }
+        return ans;
+    }
+    
+}
